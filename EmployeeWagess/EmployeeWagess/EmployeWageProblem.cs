f7cd724ec3248f46ee3663fed05c874ac914c8fe
ronitshinde <rony.shinde753@gmail.com>
2023-04-21 09:13:11 +0530

    [Ronit] Add : UC2-Check daily employee wage

--- EmployeeWagess/EmployeeWagess/EmployeWageProblem.cs
+++ EmployeeWagess/EmployeeWagess/EmployeWageProblem.cs
@@ -8,12 +8,19 @@ namespace EmployeeWagess
 {
     public class EmployeWageProblem
     {
+        const int wagePerHour = 20;
+        const int fullDayHour = 8;
+        int dailyWage = 0;
         public void Random()
         {
             Random num = new Random();
             int numOne = num.Next(2);
-            if(numOne==1)
+            if (numOne == 1)
+            {
+                dailyWage = wagePerHour * fullDayHour;
                 Console.WriteLine("Employee is present");
+                Console.WriteLine("Daily Wage is : " +dailyWage);
+            }
             else
                 Console.WriteLine("Employee is absent");
             Console.ReadKey();
