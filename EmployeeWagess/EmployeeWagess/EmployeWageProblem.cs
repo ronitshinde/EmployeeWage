d371b5c3e5dc2fa28a3e36fa2802367e510a23a4
ronitshinde <rony.shinde753@gmail.com>
2023-04-21 08:52:39 +0530

    [Ronit] Add : UC1-Check attendance of the employees

---
+++ EmployeeWagess/EmployeeWagess/EmployeWageProblem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagess
+{
+    public class EmployeWageProblem
+    {
+        public void Random()
+        {
+            Random num = new Random();
+            int numOne = num.Next(2);
+            if(numOne==1)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.ReadKey();
+        }
+    }
+}
